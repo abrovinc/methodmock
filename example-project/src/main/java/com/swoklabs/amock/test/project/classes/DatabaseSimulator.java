5f2cf0668498d5fe8f8c66ffab3a9767e2636c7a
Steve.widinghoff@jayway.com <Steve.widinghoff@jayway.com>
2016-01-27 11:05:08 +0100

    removed intellij files and folders, added them to gitignore. Started to create a example project that will run the mock with aspectj agent

---
+++ example-project/src/main/java/com/swoklabs/amock/test/project/classes/DatabaseSimulator.java
@@ -0,0 +1,21 @@
+package com.swoklabs.amock.test.project.classes;
+
+import com.swoklabs.amock.annotations.MockInTest;
+
+import java.net.ConnectException;
+
+/**
+ * Created by Steve on 2016-01-27.
+ */
+public class DatabaseSimulator {
+
+    @MockInTest(methodId = "123")
+    public Person getPersonFromDatabase(String personId) throws ConnectException {
+        throw new ConnectException("Could not connect and get");
+    }
+
+    @MockInTest(methodId = "1233")
+    public void savePersonFromDatabase(String personId) throws ConnectException {
+        throw new ConnectException("Could not connect and not save");
+    }
+}
---
+++ example-project/src/test/java/com/swoklabs/amock/test/project/PersonTest.java
@@ -0,0 +1,55 @@
+package com.swoklabs.amock.test.project;
+
+import com.swoklabs.amock.handler.MockHandler;
+import com.swoklabs.amock.model.MockContainer;
+import com.swoklabs.amock.test.project.classes.Person;
+import com.swoklabs.amock.test.project.classes.PersonController;
+import com.swoklabs.amock.test.project.classes.PersonView;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.ConnectException;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * Created by Steve on 2016-01-27.
+ */
+public class PersonTest {
+    private final PersonView personView = new PersonView();
+    private final PersonController personController = new PersonController(personView);
+
+    @Before
+    public void setup(){
+
+    }
+
+    @After
+    public void teardown(){
+
+    }
+
+    @Test
+    public void testDbException(){
+        try {
+            personController.getAndPrintPerson("abc");
+        } catch (ConnectException e) {
+            assertEquals("Could not connect and get",e.getMessage());
+        }
+    }
+
+    @Test
+    public void testMockedDbCall(){
+        final Person mockPerson = new Person("","","abc");
+        final MockContainer mockContainer = new MockContainer("123", mockPerson);
+        MockHandler mockHandler = new MockHandler();
+        mockHandler.registerMockContainer(mockContainer);
+        try {
+            personController.getAndPrintPerson("abc");
+        } catch (ConnectException e) {
+            fail("Should not throw exception");
+        }
+    }
+}
