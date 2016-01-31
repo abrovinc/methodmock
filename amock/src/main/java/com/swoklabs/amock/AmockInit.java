47969f9d5c2800ea444deeef9646fcfbd64dbaba
Nosfert <steve@widinghoff.com>
2016-01-31 01:29:01 +0100

    Agent is being copied to temp folder - need to add removal of it as well

--- amock/src/main/java/com/swoklabs/amock/AmockInit.java
+++ amock/src/main/java/com/swoklabs/amock/AmockInit.java
@@ -5,9 +5,9 @@ import com.sun.tools.attach.AgentLoadException;
 import com.sun.tools.attach.AttachNotSupportedException;
 import com.sun.tools.attach.VirtualMachine;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.lang.management.ManagementFactory;
+import java.nio.file.Files;
 
 /**
  * Created by Steve on 2016-01-27.
@@ -18,15 +18,42 @@ public class AmockInit {
         try {
             System.out.println("Loading 123");
             VirtualMachine vm = VirtualMachine.attach(getCurrentPID());
-            File file  = new File(ClassLoader.getSystemResource("aspectjweaver-1.8.8.jar").getFile());
-            System.out.println("path 12 : "+file.getAbsolutePath());
-            vm.loadAgent(file.getAbsolutePath());
+            final String totalPath = copyAgentToTempFolder();
+            System.out.println("total Path : "+totalPath);
+            vm.loadAgent(totalPath);
             vm.detach();
             System.out.println("Detaching");
         } catch (AgentLoadException | AgentInitializationException | IOException | AttachNotSupportedException e) {
             e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
+    }
+    static public String copyAgentToTempFolder() throws Exception {
+        String resourceName = "/aspectjweaver-1.8.8.jar";
+        final String tempDirectoryPath = Files.createTempDirectory("agent").toString();
+        InputStream stream = null;
+        OutputStream resStreamOut = null;
+        try {
+            stream = AmockInit.class.getResourceAsStream(resourceName);
+            if(stream == null) {
+                throw new Exception("Cannot get resource \"" + resourceName + "\" from Jar file.");
+            }
+            int readBytes;
+            byte[] buffer = new byte[4096];
+            resStreamOut = new FileOutputStream(tempDirectoryPath + resourceName);
+            while ((readBytes = stream.read(buffer)) > 0) {
+                resStreamOut.write(buffer, 0, readBytes);
+            }
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+            stream.close();
+            resStreamOut.close();
+        }
+
+        return tempDirectoryPath + resourceName;
     }
 
     private static String getCurrentPID() {
