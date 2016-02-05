ac08c00ccea6f81906824dd21a14707e2c819eeb
Nosfert <steve@widinghoff.com>
2016-02-05 22:14:12 +0100

    changed path to file

--- amock/src/main/java/com/swoklabs/amock/LoadJavaAgent.java
+++ amock/src/main/java/com/swoklabs/amock/LoadJavaAgent.java
@@ -12,9 +12,9 @@ import java.lang.management.ManagementFactory;
  * Created by Steve on 2016-01-27.
  */
 public class LoadJavaAgent {
-    private static final String resourceName = "aspectjweaver-1.8.8.jar";
+    private static final String resourceName = "/aspectjweaver-1.8.8.jar";
     private static final String tempDirBase = System.getProperty("java.io.tmpdir");
-    private static final String tempDirSufix = "/agentfolder/";
+    private static final String tempDirSufix = "/agentfolder";
     private static final String tempDirFullPath = tempDirBase + tempDirSufix;
     private static final String tempDirFullPathAndResource = tempDirBase + tempDirSufix + resourceName;
 
