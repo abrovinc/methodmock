62650be408c4dcf48158713c03e9a71b562d4da4
Nosfert <steve@widinghoff.com>
2016-02-23 16:47:28 +0100

    Removed projects

--- amock/src/test/java/com/swoklabs/amock/aspect/MockInterceptorTest.java
+++ amock/src/test/java/com/swoklabs/amock/aspect/MockInterceptorTest.java
@@ -1,7 +1,14 @@
 package com.swoklabs.amock.aspect;
 
+import org.junit.Test;
+
 /**
  * Created by Steve Widinghoff on 2016-01-31.
  */
 public class MockInterceptorTest {
+
+    @Test
+    public void mock(){
+
+    }
 }
