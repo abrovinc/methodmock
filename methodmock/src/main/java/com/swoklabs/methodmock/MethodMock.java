fe828eab7af5560f2b93d4cdf0cd827ab9c2ce85
Örjan Karlsson <orjan.karlsson@gmail.com>
2016-06-06 13:32:08 +0200

    Renamed variable in MethodMock, typo Refactored MockHandler.handleMockCall to reduce method complexity Added todo to check a enum name inn Use

--- methodmock/src/main/java/com/swoklabs/methodmock/MethodMock.java
+++ methodmock/src/main/java/com/swoklabs/methodmock/MethodMock.java
@@ -5,11 +5,13 @@ import com.swoklabs.methodmock.internal.MethodMockSpecifcationImpl;
 import com.swoklabs.methodmock.specification.MethodMockSpecifcation;
 
 public class MethodMock {
+
     public static MockHandler mockHandler = new MockHandler();
+
     public static MethodMockSpecifcation mockMethod(final String methodId){
-        final MethodMockSpecifcationImpl methodMockSpecifcation =  new MethodMockSpecifcationImpl(methodId);
-        mockHandler.registerMethodMockSpecification(methodId, methodMockSpecifcation);
-        return methodMockSpecifcation;
+        final MethodMockSpecifcationImpl methodMockSpecification =  new MethodMockSpecifcationImpl(methodId);// TODO: Spelling on variable is wrong.
+        mockHandler.registerMethodMockSpecification(methodId, methodMockSpecification);
+        return methodMockSpecification;
     }
 
     public static void clearMock(){
